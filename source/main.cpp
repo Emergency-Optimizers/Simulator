91f01d2b61346de36d7f7234b2559ed31cc8aa34
Sindre Eiklid <sindre0830@gmail.com>
2023-11-09 18:33:23 +0100

    Update `main`

--- source/main.cpp
+++ source/main.cpp
@@ -12,6 +12,7 @@
 /* internal libraries */
 #include "ODMatrix.hpp"
 #include "Incidents.hpp"
+#include "Stations.hpp"
 
 /**
  * Main program.
@@ -20,6 +21,10 @@ int main() {
     ODMatrix odMatrix;
     odMatrix.loadFromFile("../../Data-Processing/data/oslo/od_matrix.txt");
     std::cout << "Travel time from id1 to id2: " << odMatrix.getTravelTime(-1269655260, 852853940) << std::endl;
+
+    Stations stations;
+    stations.loadFromFile("../../Data-Processing/data/enhanced/oslo/depots.csv");
+    stations.printRow(2);
 
     Incidents incidents;
     incidents.loadFromFile("../../Data-Processing/data/enhanced/oslo/incidents.csv");
