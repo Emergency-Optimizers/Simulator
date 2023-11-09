f610b9eaa2673e3e4439237f65c3fc5d82f72c65
Sindre Eiklid <sindre0830@gmail.com>
2023-11-09 18:33:52 +0100

    Initial commit of `AmbulanceAllocatorStrategy`

---
+++ include/AmbulanceAllocatorStrategy.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file AmbulanceAllocatorStrategy.hpp
+ * @author Sindre Eiklid
+ * @version 1.0
+ * @date 2023-11-09
+ *
+ * @copyright Copyright (c) 2023 Sindre Eiklid
+ */
+
+#pragma once
+
+enum class AmbulanceAllocatorStrategy {
+    RANDOM,
+};
