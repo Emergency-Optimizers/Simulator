960d8eed180c118bdb2d3f4cd05d93956ef3dc18
sindre0830 <sindre0830@gmail.com>
2024-01-06 11:55:01 +0100

    Initial commit of DispatchEngineStrategy enum

--- include/DispatchEngineStrategy.hpp
+++ include/DispatchEngineStrategy.hpp
@@ -1,14 +1,14 @@
 /**
- * @file AmbulanceAllocatorStrategy.hpp
+ * @file DispatchEngineStrategy.hpp
  * @author Sindre Eiklid
  * @version 1.0
- * @date 2023-11-09
+ * @date 2024-01-06
  *
  * @copyright Copyright (c) 2023 Sindre Eiklid
  */
 
 #pragma once
 
-enum class AmbulanceAllocatorStrategy {
+enum class DispatchEngineStrategy {
     RANDOM,
 };
