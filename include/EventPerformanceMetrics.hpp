09dd4eb5356bb5b0bbf4c0d3c88a1a47be1d8fa8
Sindre Eiklid <sindre0830@gmail.com>
2024-01-15 19:42:41 +0100

    Add EventPerformanceMetrics::print

--- include/EventPerformanceMetrics.hpp
+++ include/EventPerformanceMetrics.hpp
@@ -16,4 +16,13 @@ struct EventPerformanceMetrics {
     int dispatchToHospitalTime;
     int arrivalAtHospitalTime;
     int dispatchToDepotTime;
+
+    void print() const {
+        std::cout << "Call Processed Time: " << callProcessedTime << " seconds\n";
+        std::cout << "Dispatch to Scene Time: " << dispatchToSceneTime << " seconds\n";
+        std::cout << "Arrival at Scene Time: " << arrivalAtSceneTime << " seconds\n";
+        std::cout << "Dispatch to Hospital Time: " << dispatchToHospitalTime << " seconds\n";
+        std::cout << "Arrival at Hospital Time: " << arrivalAtHospitalTime << " seconds\n";
+        std::cout << "Dispatch to Depot Time: " << dispatchToDepotTime << " seconds\n";
+    }
 };
