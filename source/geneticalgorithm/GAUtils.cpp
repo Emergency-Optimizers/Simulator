6783fbeb2e0ec69efff82bee3a81f4471a76fe88
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2024-01-15 15:44:55 +0100

    Utils class

---
+++ source/geneticalgorithm/GAUtils.cpp
@@ -0,0 +1,11 @@
+#include <random>
+
+class RandomUtil {
+public:
+    static int randomInt(int min, int max) {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(min, max);
+        return dis(gen);
+    }
+};
