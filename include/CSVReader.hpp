2c2dae79c076cd3b5b7a5deebddf26627b925cc0
Sindre Eiklid <sindre0830@gmail.com>
2024-01-15 17:06:14 +0100

    Refactor CSVReader::get

--- include/CSVReader.hpp
+++ include/CSVReader.hpp
@@ -26,23 +26,23 @@ using SchemaMapping = std::unordered_map<std::string, ToCellType>;
 
 class CSVReader {
  protected:
-    SchemaMapping schemaMapping;
-    std::vector<std::vector<CellType>> rows;
-    std::vector<std::string> headers;
+   SchemaMapping schemaMapping;
+   std::vector<std::vector<CellType>> rows;
+   std::vector<std::string> headers;
 
-    virtual void parseRow(const std::string& line);
+   virtual void parseRow(const std::string& line);
 
  public:
-    virtual ~CSVReader() = default;
-    virtual void loadFromFile(const std::string& filename);
-    const std::vector<CellType>& operator[](std::size_t index) const;
-    std::size_t size() const;
-    const std::vector<std::string>& getHeaders() const;
-    void printRow(std::size_t index);
-    void print();
-    const std::vector<std::tm> getColumnOfTimes(const std::string& header);
-    template <typename T>
-    const T get(const std::string& header, const int index) {
-      return std::get<T>(rows[index][Utils::findIndex(headers, header)]);
-    }
+   virtual ~CSVReader() = default;
+   virtual void loadFromFile(const std::string& filename);
+   const std::vector<CellType>& operator[](std::size_t index) const;
+   std::size_t size() const;
+   const std::vector<std::string>& getHeaders() const;
+   void printRow(std::size_t index);
+   void print();
+   const std::vector<std::tm> getColumnOfTimes(const std::string& header);
+   template <typename T>
+   T get(const std::string& header, const int index) {
+   return std::get<T>(rows[index][Utils::findIndex(headers, header)]);
+   }
 };
