1fdbdee8c468ccde4bb46190076c2f8807b8274f
torjus <torjus.kallekleiv@gmail.com>
2024-03-05 17:19:19 +0100

    bug fix: only 2 parents used each time

--- source/genetic-algorithm/Population.cpp
+++ source/genetic-algorithm/Population.cpp
@@ -110,7 +110,7 @@ Individual Population::crossover(const Individual& parent1, const Individual& pa
 void Population::evolve(int generations) {
     for (int gen = 0; gen < generations; gen++) {
         // step 1: parent selection
-        int numParents = 2;
+        int numParents = populationSize/2;
         int tournamentSize = 3;
         std::vector<Individual> parents = parentSelection(numParents, tournamentSize);
 
