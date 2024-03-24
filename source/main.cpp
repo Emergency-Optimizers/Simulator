0cc895bd9359f158a9245c2beef844259bfb25c2
torjus <torjus.kallekleiv@gmail.com>
2024-03-24 15:36:39 +0100

    Switch between sims in main

--- source/main.cpp
+++ source/main.cpp
@@ -15,6 +15,8 @@
 #include "simulator/MonteCarloSimulator.hpp"
 #include "metaheuristics/nsga2/Individual.hpp"
 #include "metaheuristics/nsga2/Population.hpp"
+#include "metaheuristics/genetic-algorithm/GAIndividual.hpp"
+#include "metaheuristics/genetic-algorithm/GAPopulation.hpp"
 
 int main() {
     std::mt19937 rnd(0);
@@ -26,7 +28,6 @@ int main() {
     ODMatrix odMatrix("../../Data-Processing/data/oslo/od_matrix.txt");
     std::cout << "[3/3] OD matrix loaded" << std::endl;
 
-    
     int numObjectives = 7;
     int populationSize = 30;
     int numDepots = 19;
@@ -34,15 +35,25 @@ int main() {
     double mutationProbability = 0.05;
     int generations = 30;
     bool saveEventsToCSV = true;
+    std::string simulator = "GA";
 
-    std::cout << "\nStarting NSGA..." << std::endl;
-    Population population(rnd, incidents, stations, odMatrix, populationSize, numDepots, numAmbulances, numObjectives, mutationProbability, saveEventsToCSV);
+    GAPopulation GApopulation(rnd, incidents, stations, odMatrix, populationSize, numDepots, numAmbulances, mutationProbability, saveEventsToCSV);
+    Population NSGApopulation(rnd, incidents, stations, odMatrix, populationSize, numDepots, numAmbulances, numObjectives, mutationProbability, saveEventsToCSV);
 
-    // run the NSGA for the specified number of generations
+    // run the algorithm for the specified number of generations
     auto start = std::chrono::high_resolution_clock::now();
-    population.evolve(generations);
+    if (simulator == "GA") {
+        std::cout << "\nStarting GA..." << std::endl;
+        GApopulation.evolve(generations);
+    } else if (simulator == "NSGA") {
+        std::cout << "\nStarting NSGA..." << std::endl;
+        NSGApopulation.evolve(generations);
+    }
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+
+
 
     return 0;
 }
