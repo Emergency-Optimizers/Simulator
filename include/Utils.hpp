618379fcacef82fb0ce637e3306ba9f8702fa8d4
sindre0830 <sindre0830@gmail.com>
2024-03-24 16:34:29 +0100

    Refactor Utils

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -8,7 +8,6 @@
 
 /* external libraries */
 #include <string>
-#include <ctime>
 #include <optional>
 #include <variant>
 #include <vector>
@@ -18,7 +17,6 @@
 /* internal libraries */
 #include "simulator/Ambulance.hpp"
 #include "simulator/Event.hpp"
-#include "file-reader/ODMatrix.hpp"
 
 using ValueType = std::variant<
     int,
@@ -32,7 +30,6 @@ using ValueType = std::variant<
 using ToValueType = ValueType(*)(const std::string&);
 using SchemaMapping = std::unordered_map<std::string, ToValueType>;
 
-bool tm_less(const std::tm& lhs, const std::tm& rhs);
 ValueType toInt(const std::string& str);
 ValueType toInt64(const std::string& str);
 ValueType toFloat(const std::string& str);
@@ -42,10 +39,7 @@ ValueType toBool(const std::string& str);
 ValueType toDateTime(const std::string& str);
 std::string tmToString(const std::tm& time);
 std::string valueTypeToString(const ValueType& cell);
-std::tm stringToTm(const std::string& str);
-int compareTime(const std::tm& time_1, const std::tm& time_2);
 float timeDifferenceInSeconds(std::tm& time1, std::tm& time2);
-int findClosestTimeIndex(const std::tm& target, const std::vector<std::tm>& times);
 std::vector<unsigned> getAvailableAmbulanceIndicies(
     std::vector<Ambulance>& ambulances,
     const std::vector<Event>& events,
@@ -61,8 +55,8 @@ int getRandomInt(std::mt19937& rnd, const int min, const int max);
 double getRandomProbability(std::mt19937& rnd);
 double calculateMean(const std::vector<int>& numbers);
 double calculateStandardDeviation(const std::vector<int>& numbers);
-double calculateEuclideanDistance(double x1, double y1, double x2, double y2);
-std::pair<int, int> idToUtm(int64_t grid_id);
+double calculateEuclideanDistance(const double x1, const double y1, const double x2, const double y2);
+std::pair<int, int> idToUtm(const int64_t& grid_id);
 int64_t utmToId(const std::pair<int, int>& utm, const int cellSize = 1000, const int offset = 2000000);
 int64_t approximateLocation(
     const int64_t& startId,
@@ -91,5 +85,6 @@ int findIndex(const std::vector<T>& vec, const T& value) {
 template <typename T>
 T getRandomElement(std::mt19937& rng, const std::vector<T>& vec) {
     std::uniform_int_distribution<int> rndBetween(0, vec.size() - 1);
+
     return vec[rndBetween(rng)];
 }
--- source/Utils.cpp
+++ source/Utils.cpp
@@ -6,18 +6,12 @@
 
 /* external libraries */
 #include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <optional>
-#include <vector>
 #include <algorithm>
 #include <numeric>
-#include <iterator>
 #include <chrono>
-#include <ctime>
 /* internal libraries */
 #include "Utils.hpp"
-#include "file-reader/CSVReader.hpp"
+#include "file-reader/ODMatrix.hpp"
 
 ValueType toInt(const std::string& str) {
     return std::stoi(str);
@@ -47,6 +41,7 @@ ValueType toDateTime(const std::string& str) {
     if (str.empty()) {
         return std::nullopt;
     }
+
     std::tm tm = {};
     std::istringstream ss(str);
     ss >> std::get_time(&tm, "%Y-%m-%d %H:%M:%S");
@@ -61,6 +56,7 @@ ValueType toDateTime(const std::string& str) {
 std::string tmToString(const std::tm& time) {
     std::stringstream ss;
     ss << std::put_time(&time, "%Y-%m-%d %H:%M:%S");
+
     return ss.str();
 }
 
@@ -85,63 +81,11 @@ std::string valueTypeToString(const ValueType& cell) {
     }, cell);
 }
 
-std::tm stringToTm(const std::string& str) {
-    std::tm time{};
-    std::istringstream ss(str);
-    ss >> std::get_time(&time, "%Y.%m.%dT%H:%M:%S");
-    return time;
-}
-
-int compareTime(const std::tm& time_1, const std::tm& time_2) {
-    if (time_1.tm_year > time_2.tm_year) return 1;
-    if (time_1.tm_year < time_2.tm_year) return -1;
-    if (time_1.tm_mon > time_2.tm_mon) return 1;
-    if (time_1.tm_mon < time_2.tm_mon) return -1;
-    if (time_1.tm_mday > time_2.tm_mday) return 1;
-    if (time_1.tm_mday < time_2.tm_mday) return -1;
-    if (time_1.tm_hour > time_2.tm_hour) return 1;
-    if (time_1.tm_hour < time_2.tm_hour) return -1;
-    if (time_1.tm_min > time_2.tm_min) return 1;
-    if (time_1.tm_min < time_2.tm_min) return -1;
-    if (time_1.tm_sec > time_2.tm_sec) return 1;
-    if (time_1.tm_sec < time_2.tm_sec) return -1;
-    return 0;
-}
-
 float timeDifferenceInSeconds(std::tm& time1, std::tm& time2) {
     time_t t1 = std::mktime(&time1);
     time_t t2 = std::mktime(&time2);
 
     return static_cast<float>(std::difftime(t2, t1));
-}
-
-int findClosestTimeIndex(const std::tm& target, const std::vector<std::tm>& times) {
-    if (times.empty()) return -1;
-
-    auto lower = std::lower_bound(times.begin(), times.end(), target, tm_less);
-
-    if (lower == times.end()) return times.size() - 1;
-    if (lower == times.begin()) return 0;
-
-    auto prev = lower - 1;
-
-    auto diff1 = std::mktime(const_cast<std::tm*>(&*lower)) - std::mktime(const_cast<std::tm*>(&target));
-    auto diff2 = std::mktime(const_cast<std::tm*>(&target)) - std::mktime(const_cast<std::tm*>(&*prev));
-
-    if (diff1 < diff2) {
-        return static_cast<int>(std::distance(times.begin(), lower));
-    } else {
-        return static_cast<int>(std::distance(times.begin(), prev));
-    }
-}
-
-bool tm_less(const std::tm& lhs, const std::tm& rhs) {
-    if (lhs.tm_year != rhs.tm_year) return lhs.tm_year < rhs.tm_year;
-    if (lhs.tm_mon != rhs.tm_mon) return lhs.tm_mon < rhs.tm_mon;
-    if (lhs.tm_mday != rhs.tm_mday) return lhs.tm_mday < rhs.tm_mday;
-    if (lhs.tm_hour != rhs.tm_hour) return lhs.tm_hour < rhs.tm_hour;
-    if (lhs.tm_min != rhs.tm_min) return lhs.tm_min < rhs.tm_min;
-    return lhs.tm_sec < rhs.tm_sec;
 }
 
 std::vector<unsigned> getAvailableAmbulanceIndicies(
@@ -151,7 +95,7 @@ std::vector<unsigned> getAvailableAmbulanceIndicies(
 ) {
     std::vector<unsigned> availableAmbulanceIndicies;
 
-    for (int i = 0; i < ambulances.size(); i++) {
+    for (unsigned i = 0; i < ambulances.size(); i++) {
         int eventIndex = -1;
 
         if (ambulances[i].assignedEventId != -1) {
@@ -191,11 +135,17 @@ int calculateDayDifference(const std::tm& baseDate, const int targetMonth, const
     int baseDayOfYear = baseDateNormalized.tm_yday;
     int dayDiff = 0;
 
-    if (targetDayOfYear == baseDayOfYear) return 0;
+    if (targetDayOfYear == baseDayOfYear) {
+        return 0;
+    }
 
     while (true) {
-        if (targetDayOfYear == baseDayOfYear) break;
-        if (++baseDayOfYear > totalDaysInYear) baseDayOfYear = 0;
+        if (targetDayOfYear == baseDayOfYear) {
+            break;
+        }
+        if (++baseDayOfYear > totalDaysInYear) {
+            baseDayOfYear = 0;
+        }
         dayDiff++;
     }
 
@@ -204,12 +154,18 @@ int calculateDayDifference(const std::tm& baseDate, const int targetMonth, const
     int newDayDiff = 0;
 
     while (dayDiff != newDayDiff) {
-        if (targetDayOfYear == baseDayOfYear) break;
-        if (--baseDayOfYear < 0) baseDayOfYear = totalDaysInYear;
+        if (targetDayOfYear == baseDayOfYear) {
+            break;
+        }
+        if (--baseDayOfYear < 0) {
+            baseDayOfYear = totalDaysInYear;
+        }
         newDayDiff++;
     }
 
-    if (dayDiff > newDayDiff) dayDiff = newDayDiff;
+    if (dayDiff > newDayDiff) {
+        dayDiff = newDayDiff;
+    }
 
     return dayDiff;
 }
@@ -222,35 +178,39 @@ int weightedLottery(
     std::vector<double> adjustedWeights;
 
     if (ranges.empty()) {
-        // If ranges is empty, use the entire weights vector
+        // if ranges is empty, use the entire weights vector
         adjustedWeights = weights;
     } else {
-        // Otherwise, process each specified range
+        // otherwise, process each specified range
         for (const auto& range : ranges) {
             int beginIndex = std::max(0, range.first);
             int endIndex = std::min(static_cast<int>(weights.size()) - 1, range.second);
 
             if (beginIndex <= endIndex) {
-                adjustedWeights.insert(adjustedWeights.end(), weights.begin() + beginIndex, weights.begin() + endIndex + 1);
+                adjustedWeights.insert(
+                    adjustedWeights.end(),
+                    weights.begin() + beginIndex,
+                    weights.begin() + endIndex + 1
+                );
             }
         }
     }
 
-    // Create a partial sum of the adjusted weights
+    // create a partial sum of the adjusted weights
     std::vector<double> cumulativeWeights(adjustedWeights.size());
     std::partial_sum(adjustedWeights.begin(), adjustedWeights.end(), cumulativeWeights.begin());
 
-    // Generate a random number in the adjusted range
+    // generate a random number in the adjusted range
     std::uniform_real_distribution<> dist(0.0, cumulativeWeights.back());
     auto it = std::lower_bound(cumulativeWeights.begin(), cumulativeWeights.end(), dist(rnd));
     int indexInAdjustedWeights = std::distance(cumulativeWeights.begin(), it);
 
-    // For the default case (full range), the adjusted index is the final result
+    // for the default case (full range), the adjusted index is the final result
     if (ranges.empty()) {
         return indexInAdjustedWeights;
     }
 
-    // Adjust the index to map back to the original weights vector for specified ranges
+    // adjust the index to map back to the original weights vector for specified ranges
     int originalIndex = 0;
     for (const auto& range : ranges) {
         int rangeSize = range.second - range.first + 1;
@@ -328,12 +288,12 @@ void saveDistributionToFile(const std::vector<std::vector<double>>& distribution
     auto now = std::chrono::system_clock::now();
     std::time_t now_time = std::chrono::system_clock::to_time_t(now);
 
-    // Format current time as string (YYYY-MM-DD_HH-MM-SS)
+    // format current time as string (YYYY-MM-DD_HH-MM-SS)
     std::tm bt = *std::localtime(&now_time);
     std::ostringstream oss;
     oss << std::put_time(&bt, "%Y-%m-%d_%H-%M-%S");
 
-    // Construct filename with the current date and time
+    // construct filename with the current date and time
     std::string filename =  "../data/distributions/" + baseFilename + "_" + oss.str() + ".csv";
 
     std::ofstream outFile(filename);
@@ -342,10 +302,10 @@ void saveDistributionToFile(const std::vector<std::vector<double>>& distribution
         return;
     }
 
-    // Write header
+    // write header
     outFile << "A,H,V1\n";
 
-    // Write vector to file without trailing commas
+    // write vector to file without trailing commas
     for (const auto& row : distribution) {
         for (size_t i = 0; i < row.size(); ++i) {
             outFile << row[i];
@@ -406,6 +366,7 @@ void save2dDistributionToFile(const std::vector<std::vector<double>>& distributi
 
 double getRandomProbability(std::mt19937& rnd) {
     std::uniform_real_distribution<> dist(0.0, 1.0);
+
     return dist(rnd);
 }
 
@@ -416,7 +377,7 @@ double calculateMean(const std::vector<int>& numbers) {
         sum += num;
     }
 
-    return numbers.empty() ? 0.0 : sum / numbers.size();
+    return numbers.empty() ? 0.0 : sum / static_cast<double>(numbers.size());
 }
 
 double calculateStandardDeviation(const std::vector<int>& numbers) {
@@ -424,24 +385,25 @@ double calculateStandardDeviation(const std::vector<int>& numbers) {
     double varianceSum = 0.0;
 
     for (int num : numbers) {
-        varianceSum += std::pow(num - mean, 2);
+        varianceSum += std::pow(static_cast<double>(num) - mean, 2.0);
     }
-    double variance = numbers.empty() ? 0.0 : varianceSum / numbers.size();
+    double variance = numbers.empty() ? 0.0 : varianceSum / static_cast<double>(numbers.size());
 
     return std::sqrt(variance);
 }
 
-double calculateEuclideanDistance(double x1, double y1, double x2, double y2) {
-    return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
+double calculateEuclideanDistance(const double x1, const double y1, const double x2, const double y2) {
+    return std::sqrt(std::pow(x2 - x1, 2.0) + std::pow(y2 - y1, 2.0));
 }
 
-std::pair<int, int> idToUtm(int64_t grid_id) {
+std::pair<int, int> idToUtm(const int64_t& grid_id) {
     int x = static_cast<int>(std::floor(static_cast<double>(grid_id) * std::pow(10, -7)) - (2 * std::pow(10, 6)));
     int y = static_cast<int>(static_cast<double>(grid_id) - (std::floor(static_cast<double>(grid_id) * std::pow(10, -7)) * std::pow(10, 7)));
+
     return std::make_pair(x, y);
 }
 
-int64_t utmToId(const std::pair<int, int>& utm, int cellSize, int offset) {
+int64_t utmToId(const std::pair<int, int>& utm, const int cellSize, const int offset) {
     int64_t xCorner = std::floor((utm.first + offset) / cellSize) * cellSize - offset;
     int64_t yCorner = std::floor(utm.second / cellSize) * cellSize;
 
@@ -488,7 +450,9 @@ int64_t approximateLocation(
 
 int findEventIndexFromId(const std::vector<Event>& events, const int id) {
     for (int i = 0; i < events.size(); i++) {
-        if (events[i].id == id) return i;
+        if (events[i].id == id) {
+            return i;
+        }
     }
 
     return -1;
