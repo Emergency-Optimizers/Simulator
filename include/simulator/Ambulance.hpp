489d20cc259b37c9b819673613663bdc004224bf
sindre0830 <sindre0830@gmail.com>
2024-03-24 17:11:16 +0100

    Refactor Ambulance

--- include/simulator/Ambulance.hpp
+++ include/simulator/Ambulance.hpp
@@ -10,12 +10,11 @@
 #include <vector>
 /* internal libraries */
 #include "simulator/Event.hpp"
-#include "simulator/EventType.hpp"
 
 struct Ambulance {
     int id = -1;
     int allocatedDepotIndex = -1;
-    int64_t currentGridId = -1;
+    int64_t currentGridId = -1i64;
     int assignedEventId = -1;
     int timeUnavailable = 0;
     int timeNotWorking = 0;
@@ -23,74 +22,20 @@ struct Ambulance {
     int breakLenght = 0;
     std::vector<time_t> scheduledBreaks;
 
-    void checkScheduledBreak(const time_t& currentTime) {
-        if (!scheduledBreaks.empty() && currentTime >= scheduledBreaks.front()) {
-            int breakLength = 30 * 60;
+    void checkScheduledBreak(const time_t& currentTime);
 
-            setBreak(breakLength, currentTime);
-
-            scheduledBreaks.erase(scheduledBreaks.begin());
-        }
-    }
-
-    void setBreak(const int newBreakLength, const time_t& currentTime) {
-        timeBreakStarted = currentTime;
-        breakLenght = newBreakLength;
-    }
+    void setBreak(const int newBreakLength, const time_t& currentTime);
 
     bool isAvailable(
         const std::vector<Event>& events,
         const int eventIndex,
         const time_t& currentTime
-    ) {
-        // if the ambulance is on a break and it is not finished, mark it as unavailable
-        if (breakLenght == 0) {
-            checkScheduledBreak(currentTime);
-        }
-        if (breakLenght != 0) {
-            if (currentTime >= timeBreakStarted + breakLenght) {
-                timeNotWorking += currentTime - timeBreakStarted;
-                timeBreakStarted = 0;
-                breakLenght = 0;
-
-                return true;
-            } else {
-                return false;
-            }
-        }
-        if (assignedEventId == -1) {
-            return true;
-        }
-        if (events[eventIndex].type == EventType::FINISHED) {
-            return true;
-        }
-        return false;
-    }
+    );
 
     void scheduleBreaks(
         const time_t& shiftStart,
         const time_t& shiftEnd,
         const int depotSize,
         const int depotNum
-    ) {
-        time_t shiftLength = shiftEnd - shiftStart;
-        // constraints: at least 1 hour after shift starts and 1 hour before shift ends. minimum 4 hours between each break
-        time_t firstHourEnd = shiftStart + 3600;
-        time_t lastHourStart = shiftEnd - 3600;
-        time_t minBreakInterval = 14400;
-
-        time_t break1Start = firstHourEnd + (depotNum % depotSize) * (minBreakInterval / depotSize);
-        if (break1Start > lastHourStart - 1800) {
-            break1Start = firstHourEnd;
-        }
-
-        time_t break2Start = break1Start + minBreakInterval;
-
-        if (break2Start + 1800 > lastHourStart) {
-            break2Start = lastHourStart - 1800;
-        }
-
-        scheduledBreaks.push_back(break1Start);
-        scheduledBreaks.push_back(break2Start);
-    }
+    );
 };
---
+++ source/simulator/Ambulance.cpp
@@ -0,0 +1,79 @@
+/**
+ * @file Ambulance.cpp
+ *
+ * @copyright Copyright (c) 2024 Emergency-Optimizers
+ */
+
+/* internal libraries */
+#include "simulator/Ambulance.hpp"
+
+void Ambulance::checkScheduledBreak(const time_t& currentTime) {
+    if (!scheduledBreaks.empty() && currentTime >= scheduledBreaks.front()) {
+        int breakLength = 30 * 60;
+
+        setBreak(breakLength, currentTime);
+
+        scheduledBreaks.erase(scheduledBreaks.begin());
+    }
+}
+
+void Ambulance::setBreak(const int newBreakLength, const time_t& currentTime) {
+    timeBreakStarted = currentTime;
+    breakLenght = newBreakLength;
+}
+
+bool Ambulance::isAvailable(
+    const std::vector<Event>& events,
+    const int eventIndex,
+    const time_t& currentTime
+) {
+    // if the ambulance is on a break and it is not finished, mark it as unavailable
+    if (breakLenght == 0) {
+        checkScheduledBreak(currentTime);
+    }
+    if (breakLenght != 0) {
+        if (currentTime >= timeBreakStarted + breakLenght) {
+            timeNotWorking += currentTime - timeBreakStarted;
+            timeBreakStarted = 0;
+            breakLenght = 0;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if (assignedEventId == -1) {
+        return true;
+    }
+    if (events[eventIndex].type == EventType::FINISHED) {
+        return true;
+    }
+    return false;
+}
+
+void Ambulance::scheduleBreaks(
+    const time_t& shiftStart,
+    const time_t& shiftEnd,
+    const int depotSize,
+    const int depotNum
+) {
+    time_t shiftLength = shiftEnd - shiftStart;
+    // constraints: at least 1 hour after shift starts and 1 hour before shift ends. minimum 4 hours between each break
+    time_t firstHourEnd = shiftStart + 3600;
+    time_t lastHourStart = shiftEnd - 3600;
+    time_t minBreakInterval = 14400;
+
+    time_t break1Start = firstHourEnd + (depotNum % depotSize) * (minBreakInterval / depotSize);
+    if (break1Start > lastHourStart - 1800) {
+        break1Start = firstHourEnd;
+    }
+
+    time_t break2Start = break1Start + minBreakInterval;
+
+    if (break2Start + 1800 > lastHourStart) {
+        break2Start = lastHourStart - 1800;
+    }
+
+    scheduledBreaks.push_back(break1Start);
+    scheduledBreaks.push_back(break2Start);
+}
