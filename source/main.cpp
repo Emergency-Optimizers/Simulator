2ff1bcf69174c36306c731eb092ec81a64a8dcca
sindre0830 <sindre0830@gmail.com>
2024-03-31 17:40:51 +0200

    Add functionality to run simulator once

--- source/main.cpp
+++ source/main.cpp
@@ -57,6 +57,60 @@ int main() {
             Settings::get<bool>("SIMULATE_DAY_SHIFT")
         );
         population.evolve(Settings::get<int>("GENERATION_SIZE"));
+    } else if (heuristic == "NONE") {
+        // generate events
+        MonteCarloSimulator monteCarloSim(
+            rnd,
+            Settings::get<int>("SIMULATE_YEAR"),
+            Settings::get<int>("SIMULATE_MONTH"),
+            Settings::get<int>("SIMULATE_DAY"),
+            Settings::get<bool>("SIMULATE_DAY_SHIFT"),
+            Settings::get<int>("SIMULATION_GENERATION_WINDOW_SIZE")
+        );
+        std::vector<Event> events = monteCarloSim.generateEvents();
+
+        // set allocation
+        std::vector<std::vector<int>> allocations;
+        allocations.push_back({2, 4, 2, 2, 2, 4, 2, 3, 3, 4, 4, 6, 4, 3, 3, 0, 0, 0, 0});
+
+        AmbulanceAllocator ambulanceAllocator;
+        ambulanceAllocator.allocate(
+            events,
+            allocations,
+            Settings::get<bool>("SIMULATE_DAY_SHIFT")
+        );
+
+        // simulate events
+        Simulator simulator(
+            rnd,
+            ambulanceAllocator,
+            Settings::get<DispatchEngineStrategyType>("DISPATCH_STRATEGY"),
+            events
+        );
+        simulator.run();
+
+        // print metrics
+        double avgResponseTimeAUrban = simulator.averageResponseTime("A", true);
+        double avgResponseTimeANonurban = simulator.averageResponseTime("A", false);
+        double avgResponseTimeHUrban = simulator.averageResponseTime("H", true);
+        double avgResponseTimeHNonurban = simulator.averageResponseTime("H", false);
+        double avgResponseTimeV1Urban = simulator.averageResponseTime("V1", true);
+        double avgResponseTimeV1Nonurban = simulator.averageResponseTime("V1", false);
+
+        std::cout
+            << "Goal:" << std::endl
+            << "\t A, urban: <12 min" << std::endl
+            << "\t A, non-urban: <25 min" << std::endl
+            << "\t H, urban: <30 min" << std::endl
+            << "\t H, non-urban: <40 min" << std::endl
+            << std::endl
+            << "Avg. response time (A, urban): \t\t" << avgResponseTimeAUrban << "s (" << avgResponseTimeAUrban / 60 << "m)" << std::endl
+            << "Avg. response time (A, non-urban): \t" << avgResponseTimeANonurban << "s (" << avgResponseTimeANonurban / 60 << "m)" << std::endl
+            << "Avg. response time (H, urban): \t\t" << avgResponseTimeHUrban << "s (" << avgResponseTimeHUrban / 60 << "m)" << std::endl
+            << "Avg. response time (H, non-urban): \t" << avgResponseTimeHNonurban << "s (" << avgResponseTimeHNonurban / 60 << "m)" << std::endl
+            << "Avg. response time (V1, urban): \t" << avgResponseTimeV1Urban << "s (" << avgResponseTimeV1Urban / 60 << "m)" << std::endl
+            << "Avg. response time (V1, non-urban): \t" << avgResponseTimeV1Nonurban << "s (" << avgResponseTimeV1Nonurban / 60 << "m)" << std::endl
+            << "Total violations: \t\t\t" << simulator.responseTimeViolations() << std::endl;
     } else {
         std::cout << "Unknown heuristic given." << std::endl;
     }
