5436668a13ec201a0eb0e6b294eb350e134887a7
sindre0830 <sindre0830@gmail.com>
2024-06-09 09:02:02 +0200

    Add code documentation to Traffic.hpp

--- source/file-reader/Traffic.cpp
+++ source/file-reader/Traffic.cpp
@@ -10,6 +10,7 @@
 #include "file-reader/Traffic.hpp"
 
 Traffic::Traffic() {
+    // define the schema: header and function that converts string to specific type
     schemaMapping = {
         {"Monday", toDouble},
         {"Tuesday", toDouble},
@@ -28,5 +29,7 @@ double Traffic::getTrafficFactor(const time_t& time) {
 
     const char* daysOfWeek[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+    // returns the traffic factor based on day of week, and hour of day
+    // traffic factor is used with the OD cost matrix to increase realism
     return get<double>(daysOfWeek[localTime.tm_wday], localTime.tm_hour);
 }
